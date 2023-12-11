2b82954077adec43abe082ada006cd452f0f34ea
PappNiki20 <papp.nikoletta001@gmail.com>
2023-12-11 08:48:49 +0100

    karakzter alapja

---
+++ gamyyy/Karakter.java
@@ -0,0 +1,14 @@
+
+package gamyyy;
+
+public class Karakter {
+    int eletero, ugyesseg;
+    boolean szerencse;
+
+    public Karakter(int eletero, int ugyesseg, boolean szerencse) {
+        this.eletero = eletero;
+        this.ugyesseg = ugyesseg;
+        this.szerencse = szerencse;
+    }
+    
+}
