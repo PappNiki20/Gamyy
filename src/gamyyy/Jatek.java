db9a5901e2e59dd0fad35e52a197a034d767a20c
PappNiki20 <papp.nikoletta001@gmail.com>
2023-12-11 10:22:03 +0100

    hasznal metodus keszen

--- src/gamyyy/Jatek.java
+++ src/gamyyy/Jatek.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gamyyy;
 
-/**
- *
- * @author papp.nikoletta
- */
 public class Jatek {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        Karakter k1 = new Karakter("Leiahercegnő");
+        Karakter k2 = new Karakter("ZoéTünde");
+        Targy t1 = new Targy("kulacs", "harcifegyver");
+         Targy t2 = new Targy("kinderjoy", "étel");
+         Karakter[] kk = {k1, k2};
+          Targy[] tt = {t1, t2};
+        kiirTomb("Karakterek :",kk);
+        kiirTomb("Felszedhető tárgyak", tt);
+        k1.felvesz(t1);
+        k2.felvesz(t2, 3);
+        kiirTomb("Karakterek felszerelésekkel:",kk);
+        k2.hasznal("kinderjoy");
+        kiirTomb("Karakterek felszerelésekkel:",kk);
+    }
+
+     private static <T> void kiirTomb(String cim, T[] tomb) {
+        System.out.println(cim);
+        for (T elem : tomb) {
+            System.out.println(elem);
+        }
     }
     
 }
--- src/gamyyy/Karakter.java
+++ src/gamyyy/Karakter.java
@@ -1,17 +1,70 @@
-
 package gamyyy;
 
-public class Karakter {
-    int eletero, ugyesseg, szerencse;
-    String[] felszereles;
+import java.util.ArrayList;
 
-    public Karakter(int eletero, int ugyesseg, int szerencse, String[] felszereles) {
-        this.eletero = eletero;
-        this.ugyesseg = ugyesseg;
-        this.szerencse = szerencse;
-        this.felszereles = felszereles;
+public class Karakter {
+
+    private int eletero, ugyesseg, szerencse;
+    private ArrayList<Targy> felszereles;
+    String nev;
+
+    public Karakter(String nev) {
+        this.nev = nev;
+        this.eletero = (kockadobas() + kockadobas()) + 12;
+        this.ugyesseg = kockadobas() + 6;
+        this.szerencse = kockadobas() + 6;
+        this.felszereles = new ArrayList<Targy>();
+
+        //  this.felszereles = felszereles;
     }
-    
-   
-    
+
+    private int kockadobas() {
+        int kocka = (int) (Math.random() * 6 - 1 + 1) + 1;
+        return kocka;
+    }
+
+    @Override
+    public String toString() {
+        return "Karakter{" + "eletero=" + eletero + ", ugyesseg=" + ugyesseg + ", szerencse=" + szerencse + ", felszereles=" + felszereles + ", nev=" + nev + '}';
+    }
+
+    public ArrayList<Targy> felvesz(Targy t) {
+        felszereles.add(t);
+        return felszereles;
+    }
+
+    public ArrayList<Targy> felvesz(Targy t, int db) {
+        for (int i = 0; i < db; i++) {
+            felszereles.add(t);
+        }
+
+        return felszereles;
+    }
+
+    public void hasznal(String nev) {
+        int i = 0, N = felszereles.size();
+        while (i < N && !(felszereles.get(i).getNev().equals(nev))) {
+            i++;
+        }
+        if (i < N) {
+            felszereles.remove(i);
+        } else {
+            System.out.println("A keresett tárgy nem használható!!!");
+        }
+    }
+
+    public void hasznal(String nev, int db) {
+        for (int i = 0; i < db; i++) {
+            hasznal(nev);
+        }
+    }
+    public void hasznal(String nev, int db, boolean levonjae){
+      if(levonjae == true){
+          hasznal(nev, db);
+      }
+      else{
+          System.out.println("Nem rendelkezel ennyi tárggyal!!");
+      }
+    }
+
 }
