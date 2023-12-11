3234eb9baa5347cd24aec1f1d71ada934e0daee9
PappNiki20 <papp.nikoletta001@gmail.com>
2023-12-11 08:57:43 +0100

    jatek alapjai

---
+++ src/gamyyy/Karakter.java
@@ -0,0 +1,17 @@
+
+package gamyyy;
+
+public class Karakter {
+    int eletero, ugyesseg, szerencse;
+    String[] felszereles;
+
+    public Karakter(int eletero, int ugyesseg, int szerencse, String[] felszereles) {
+        this.eletero = eletero;
+        this.ugyesseg = ugyesseg;
+        this.szerencse = szerencse;
+        this.felszereles = felszereles;
+    }
+    
+   
+    
+}
